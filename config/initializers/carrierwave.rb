1cf8a93d808847992896e587d3517506a02af9fa
Bill Holmes <bill.n.holmes@gmail.com>
2019-07-23 00:22:28 +0000

    Almost got AWS to work

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,12 @@
 #config/initionlizers/carrierwave.rb
 
 CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'
+  config.fog_provider = 'fog/aws'                        # required
   config.fog_public = false
-  config.fog_credentials ={
-    provider: 'AWS',
-    aws_access_key_id:        ENV[AKIAV4D5EVNVXQN4BE5P],
-    aws_secret_access_key:    ENV[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],
+  config.fog_credentials = {
+    provider:              'AWS',                        # required
+    aws_access_key_id:     ENV[AKIAV4D5EVNV2IMFPVBT],        # required
+    aws_secret_access_key: ENV[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],        # required
   }
-  config.fog_directory =      ENV[photopicture]
+  config.fog_directory  = ENV["photopicture"]              # required
 end
